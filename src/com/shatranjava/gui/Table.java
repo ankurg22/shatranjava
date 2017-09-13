a7acc858b76374f36428c54ea3f1c610b547a795
Ankur Gupta <guptaankur.gupta0@gmail.com>
2017-09-13 22:28:28 +0530

    Implemented Tile and Board Panels

--- src/com/shatranjava/gui/Table.java
+++ src/com/shatranjava/gui/Table.java
@@ -1,19 +1,27 @@
 package com.shatranjava.gui;
 
+import com.shatranjava.engine.Coordinate;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Table {
-
     private final JFrame mGameFrame;
+    private final BoardPanel mBoardPanel;
 
     private static final Dimension FRAME_DIMENSION_OUTER = new Dimension(600, 600);
+    private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
+    private static final Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
 
     public Table() {
         mGameFrame = new JFrame("ShatranJava");
+        mGameFrame.setLayout(new BorderLayout());
         final JMenuBar tableMenuBar = createTableMenuBar();
         mGameFrame.setJMenuBar(tableMenuBar);
         mGameFrame.setSize(FRAME_DIMENSION_OUTER);
+
+        mBoardPanel = new BoardPanel();
+        mGameFrame.add(mBoardPanel, BorderLayout.CENTER);
         mGameFrame.setVisible(true);
     }
 
@@ -33,5 +41,43 @@ public class Table {
 
         fileMenu.add(openPGN);
         return fileMenu;
+    }
+
+    private class BoardPanel extends JPanel {
+        final TilePanel[][] boardTiles;
+
+        public BoardPanel() {
+            super(new GridLayout(8, 8));
+            boardTiles = new TilePanel[8][8];
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    final TilePanel tilePanel = new TilePanel(this, new Coordinate(i, j));
+                    boardTiles[i][j] = tilePanel;
+                    add(tilePanel);
+                }
+            }
+            setPreferredSize(BOARD_PANEL_DIMENSION);
+            validate();
+        }
+    }
+
+    private class TilePanel extends JPanel {
+
+        private final Coordinate tileCoordinate;
+
+        public TilePanel(final BoardPanel boardPanel,
+                         final Coordinate tileCoordinate) {
+            super(new GridBagLayout());
+            this.tileCoordinate = tileCoordinate;
+            setPreferredSize(TILE_PANEL_DIMENSION);
+            assignTileColor();
+            validate();
+        }
+
+        private void assignTileColor() {
+
+        }
+
+
     }
 }
